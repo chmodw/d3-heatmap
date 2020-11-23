40f2ef0786c3f0a81ce8a2bcf4e06ec42ab23a60
chamodya wimansha <chamodyawimansha@gmail.com>
2020-11-23 19:35:32 +0530

    temps variable crated to store calculated temps

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
-const margin = { top: 50, right: 20, bottom: 50, left: 80 };
-const width = 1000 - margin.left - margin.right;
+const margin = { top: 50, right: 70, bottom: 50, left: 80 };
+const width = 1200 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 
 const monthNames = [
@@ -31,6 +31,9 @@ d3.json(
   .then((data) => {
     const years = d3.map(data.monthlyVariance, (d) => d.year);
     const months = d3.map(data.monthlyVariance, (d) => d.month);
+    const temps = data.monthlyVariance.map(
+      (d) => data.baseTemperature + d.variance
+    );
 
     // adding x and y axis
     let xAxis = d3.scaleBand().range([0, width]).domain(years);
@@ -56,22 +59,20 @@ d3.json(
     var colors = d3
       .scaleSequential()
       .interpolator(d3.interpolateInferno)
-      .domain([
-        d3.min(data.monthlyVariance.map((d) => d.variance)),
-        d3.max(data.monthlyVariance.map((d) => d.variance)),
-      ]);
+      .domain([d3.min(temps), d3.max(temps)]);
 
-    // add the squares
+    // Adding the data to the chart
     chart
       .selectAll()
       .data(data.monthlyVariance, (d) => d.year + ":" + d.month)
       .enter()
       .append("rect")
+      .attr("class", "cell")
       .attr("x", (d) => xAxis(d.year))
       .attr("y", (d) => yAxis(d.month))
       .attr("width", xAxis.bandwidth())
       .attr("height", yAxis.bandwidth())
-      .style("fill", (d) => colors(d.variance));
+      .style("fill", (d) => colors(data.baseTemperature + d.variance));
   })
   .catch((error) => {
     if (error) throw error;
